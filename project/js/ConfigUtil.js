af6bcfc40db2861da82b179260ad26e610decfa8
15075767858 <a5461789>
2016-03-03 16:22:28 +0800

    合并完成

--- project/js/ConfigUtil.js
+++ project/js/ConfigUtil.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2016/2/21.
  */
-function parseINIString(data){
+function parseINIString(data) {
     var regex = {
         section: /^\s*\s*([^]*)\s*\]\s*$/,
         param: /^\s*([\w\.\-\_]+)\s*=\s*(.*?)\s*$/,
@@ -10,23 +10,24 @@ function parseINIString(data){
     var value = {};
     var lines = data.split(/\r\n|\r|\n/);
     var section = null;
-    lines.forEach(function(line){
-        if(regex.comment.test(line)){
+    lines.forEach(function (line) {
+        if (regex.comment.test(line)) {
             return;
-        }else if(regex.param.test(line)){
+        } else if (regex.param.test(line)) {
             var match = line.match(regex.param);
-            if(section){
+            if (section) {
                 value[section][match[1]] = match[2];
-            }else{
+            } else {
                 value[match[1]] = match[2];
             }
-        }else if(regex.section.test(line)){
+        } else if (regex.section.test(line)) {
             var match = line.match(regex.section);
             value[match[1]] = {};
             section = match[1];
-        }else if(line.length == 0 && section){
+        } else if (line.length == 0 && section) {
             section = null;
-        };
+        }
+        ;
     });
     return value;
 }
@@ -63,7 +64,7 @@ xmlToStr = function (xmlDom) {
         return new XMLSerializer().serializeToString(xmlDom);
     }
 }
-function showXml(fileName,success) {
+function showXml(fileName, success) {
     $.ajax({
         type: "POST",
         url: "xmlRW.php",
@@ -72,11 +73,11 @@ function showXml(fileName,success) {
     });
 }
 
-function saveXml(fileName,success,content){
+function saveXml(fileName, success, content) {
     $.ajax({
         type: "POST",
         url: "xmlRW.php",
-        data: "fileName="+fileName+"&content=" + content + "&rw=w",
+        data: "fileName=" + fileName + "&content=" + content + "&rw=w",
         success: success
     });
 }
@@ -86,3 +87,126 @@ function isIE() { //ie?
     else
         return false;
 }
+
+function xmlToJson(xml) {
+    // Create the return object
+    var obj = {};
+    if (xml.nodeType == 1) { // element
+        // do attributes
+        if (xml.attributes.length > 0) {
+            obj["@attributes"] = {};
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var attribute = xml.attributes.item(j);
+                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+            }
+        }
+    } else if (xml.nodeType == 3) { // text
+        obj = xml.nodeValue;
+    }
+    // do children
+    if (xml.hasChildNodes()) {
+        for (var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nodeName = item.nodeName;
+            if (typeof(obj[nodeName]) == "undefined") {
+                obj[nodeName] = xmlToJson(item);
+            } else {
+                if (typeof(obj[nodeName].length) == "undefined") {
+                    var old = obj[nodeName];
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                }
+                obj[nodeName].push(xmlToJson(item));
+            }
+        }
+    }
+    return obj;
+};
+
+//思路：函数接收到一个XML DOM对象
+//如果节点类型=1 获得它的节点名称
+//获取它的属性如果数量大于0 就把它存入json
+//子节点数量 如果子节点数量等于0 就建一个数组 对这个子元素进行递归 保存这个子节点下所有的子节点
+//
+//console.log(XmlDom.childNodes[0].attributes['abc'].value)
+//
+
+function XmlToFrameJson(XmlDom) {
+    var jsonobj = {};
+    if (XmlDom.nodeType != 1) {
+
+        //jsonobj['name'] = XmlDom.childNodes[0].tagName;
+        //XmlToFrameJson(XmlDom.childNodes[0])
+    }
+    var oNewChildNode = [];
+    for (var i = 0; i < XmlDom.childNodes.length; i++) {
+        jsonobj['name'] = XmlDom.tagName;
+        if (XmlDom.childNodes[i].nodeType == 1) {
+            var childNode = XmlDom.childNodes[i];
+            oNewChildNode.push(XmlDom.childNodes[i]);
+
+            var attrs = XmlDom.attributes;
+            if (attrs.length > 0) {
+                for (attr in attrs) {
+                    if (attr && attrs[attr].value && attrs[attr]) {
+                        //console.log(attrs[attr].name + ":" + attrs[attr].value);
+                        jsonobj[attrs[attr].name] = attrs[attr].value;
+                    }
+                }
+            }
+            //console.log(childNode.childNodes);
+            // var aType1 = childNode.childNodes;
+            if (oNewChildNode.length > 0) {
+                jsonobj["children"] = [];
+                for (var j = 0; j < oNewChildNode.length; j++) {
+                        jsonobj["children"].push(XmlToFrameJson(oNewChildNode[j]));
+                }
+            }
+        }
+    }
+    return jsonobj;
+}
+function deleteXmlNodeByIndex(XmlDom,Index){
+    var aDoms=XmlDom.querySelectorAll("*");
+    console.log(aDoms);
+    aDoms[Index].parentNode.removeChild(aDoms[Index]);
+    return XmlDom;
+}
+function addXmlNodeByIndex(XmlDom,Index,oEle){
+    var aDoms=XmlDom.querySelectorAll("*");
+    console.log(aDoms);
+    aDoms[Index].appendChild(oEle);
+    return XmlDom;
+};
+
+
+/*
+function deleteXmlNodeByIndex(XmlDom,Index){
+    var indexCount=0;
+    findDom(XmlDom,Index);
+    function findDom(XmlDom,Index){
+
+    for (var i = 0; i < XmlDom.childNodes.length; i++) {
+        if (XmlDom.childNodes[i].nodeType == 1) {
+            console.log(XmlDom.childNodes[i])
+            indexCount++;
+
+        }
+    }
+    }
+    return XmlDom;
+}*/
+
+
+/*
+ //var name = childNode.tagName;
+ //jsonobj['name'] = XmlDom.childNodes[i].tagName;
+ //console.log(name)
+
+ var iCount=0;
+ for(var k=0;k<childNode.childNodes.length;k++){
+ if(childNode.childNodes[k].nodeType==1){
+ oNewChildNode[iCount]=childNode.childNodes[k];
+ iCount++;
+ }
+ }*/
